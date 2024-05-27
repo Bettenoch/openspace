0d6b8341592ebec85f38aa842a91e799d1c286eb
Bettenoch <bettenoch254@gmail.com>
2024-05-27 19:36:22 +0300

    Remove fields not in the user model.

--- chat/user/serializers.py
+++ chat/user/serializers.py
@@ -9,5 +9,5 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'bio', 'avatar', 'is_active', 'created', 'updated')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'created', 'updated')
         read_only_field = ['is_active']
