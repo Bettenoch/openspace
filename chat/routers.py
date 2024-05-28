3e284e9fbe858e29e02d81cada324084d243a7dc
Bettenoch <bettenoch254@gmail.com>
2024-05-28 15:25:10 +0300

    add refresh viewset to routes.

--- chat/routers.py
+++ chat/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from chat.user.viewsets import UserViewSet
-from chat.auth.viewsets import RegisterViewSet, LoginViewSet
+from chat.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 from django.urls import path, include
 
 router = routers.SimpleRouter()
@@ -10,6 +10,7 @@ router = routers.SimpleRouter()
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 urlpatterns = [
     *router.urls,
