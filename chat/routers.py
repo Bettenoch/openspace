8d7a825f2bfb15e19a05901eda0ef47ac513c4ca
Bettenoch <bettenoch254@gmail.com>
2024-05-28 13:09:31 +0300

    create login route.

--- chat/routers.py
+++ chat/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from chat.user.viewsets import UserViewSet
-from chat.auth.viewsets.register import RegisterViewSet
+from chat.auth.viewsets import RegisterViewSet, LoginViewSet
 from django.urls import path, include
 
 router = routers.SimpleRouter()
@@ -9,6 +9,7 @@ router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 urlpatterns = [
     *router.urls,
