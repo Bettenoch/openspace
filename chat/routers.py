c1552313ae45f9dd0e35801ea755ef28f65fc63f
Bettenoch <bettenoch254@gmail.com>
2024-05-28 09:18:53 +0300

    add register router.

--- chat/routers.py
+++ chat/routers.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from chat.user.viewsets import UserViewSet
+from chat.auth.viewsets.register import RegisterViewSet
 from django.urls import path, include
 
 router = routers.SimpleRouter()
@@ -7,6 +8,7 @@ router = routers.SimpleRouter()
 ############################# USER ###################################
 
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 
 urlpatterns = [
     *router.urls,
