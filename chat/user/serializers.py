ba2cf4478e7ddd570244399ec910ecef1aebb4df
Bettenoch <bettenoch254@gmail.com>
2024-05-28 09:17:03 +0300

    Format code.

--- chat/user/serializers.py
+++ chat/user/serializers.py
@@ -2,12 +2,22 @@ from rest_framework import serializers
 
 from chat.user.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
+    id = serializers.UUIDField(source="public_id", read_only=True, format="hex")
     created = serializers.DateTimeField(read_only=True)
     updated = serializers.DateTimeField(read_only=True)
-    
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'created', 'updated')
-        read_only_field = ['is_active']
+        fields = (
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "is_active",
+            "created",
+            "updated",
+        )
+        read_only_field = ["is_active"]
