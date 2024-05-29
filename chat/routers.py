88ec7b53e0633aff54215f64225b0b099ff3396a
Bettenoch <bettenoch254@gmail.com>
2024-05-29 10:18:41 +0300

    add post viewsset to routes

--- chat/routers.py
+++ chat/routers.py
@@ -1,13 +1,14 @@
 from rest_framework import routers
 from chat.user.viewsets import UserViewSet
 from chat.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
-from django.urls import path, include
+from chat.post.viewsets import PostViewSet
 
 router = routers.SimpleRouter()
 
 ############################# USER ###################################
 
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'post', PostViewSet, basename='post')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
