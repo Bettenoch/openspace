586d963c4a22d6faacdacd8a53f6a522f6256037
Bettenoch <bettenoch254@gmail.com>
2024-05-30 09:05:59 +0300

    created comment nested route from post.

--- chat/routers.py
+++ chat/routers.py
@@ -1,18 +1,24 @@
-from rest_framework import routers
 from chat.user.viewsets import UserViewSet
 from chat.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 from chat.post.viewsets import PostViewSet
+from chat.comment.viewsets import CommentViewSet
+from rest_framework_nested import routers
 
 router = routers.SimpleRouter()
 
 ############################# USER ###################################
 
 router.register(r'user', UserViewSet, basename='user')
-router.register(r'post', PostViewSet, basename='post')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
+router.register(r'post', PostViewSet, basename='post')
+
+posts_router = routers.NestedSimpleRouter(router, r'post', lookup='post')
+posts_router.register(r'comment', CommentViewSet, basename='post-comment')
+
 urlpatterns = [
     *router.urls,
+    *posts_router.urls,
 ]
