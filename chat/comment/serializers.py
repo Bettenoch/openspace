93c3a920cae694567d71e8efa6bc409fc37332a5
Bettenoch <bettenoch254@gmail.com>
2024-05-30 09:05:37 +0300

    create comment serializer with to representation method.

---
+++ chat/comment/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from chat.abstract.serializers import AbstractSerializer
+from chat.user.models import User
+from chat.user.serializers import UserSerializer
+from chat.comment.models import Comment
+from chat.post.models import Post
+
+
+class CommentSerializer(AbstractSerializer):
+    author = serializers.SlugRelatedField(
+        queryset=User.objects.all(), slug_field="public_id"
+    )
+
+    post = serializers.SlugRelatedField(
+        queryset=Post.objects.all(), slug_field="public_id"
+    )
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        author = User.objects.get_object_by_public_id(rep["author"])
+        rep["author"] = UserSerializer(author).data
+
+        return rep
+
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "post",
+            "author",
+            "body",
+            "created",
+            "updated",
+            "edited",
+        ]
+
+        read_only_fields = ["edited"]
