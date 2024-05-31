d8fb90b6b6f3fb846864e101b4141d0d4a9a9d63
Bettenoch <bettenoch254@gmail.com>
2024-05-31 10:48:21 +0300

    write test for the comment model.

--- chat/comment/tests.py
+++ chat/comment/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from chat.fixtures.user import user
+from chat.fixtures.post import post
+from chat.comment.models import Comment
+
+@pytest.mark.django_db
+def test_comment(user, post):
+    comment = Comment.objects.create(
+        post=post,
+        author=user,
+        body="This is a test comment"
+    )
+    assert comment.post == post
+    assert comment.author == user
+    assert comment.body == "This is a test comment"
+    assert comment.edited == False
