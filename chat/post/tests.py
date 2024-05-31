8e5173743f9f1f31788477388e746e8d3a85f302
Bettenoch <bettenoch254@gmail.com>
2024-05-31 10:29:21 +0300

    test the post model.

--- chat/post/tests.py
+++ chat/post/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import pytest
+from chat.fixtures.user import user
+from chat.post.models import Post
+
 
 # Create your tests here.
+@pytest.mark.django_db
+def test_create_post(user):
+    post = Post.objects.create(author=user, body="Should be  a post")
+    assert post.author == user
+    assert post.body == "Should be  a post"
