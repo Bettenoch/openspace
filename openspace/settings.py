8ec4ef920ca6fcad373f2ded017d03719acc7cee
Bettenoch <bettenoch254@gmail.com>
2024-06-22 12:43:31 +0300

    use cors headers to allow urls fetch

--- openspace/settings.py
+++ openspace/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'chat',
     'chat.user',
     'chat.auth',
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'openspace.urls'
@@ -147,3 +149,9 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
