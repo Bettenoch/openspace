15ef3f2592dfd92d1446eba8b290f57322101010
Bettenoch <bettenoch254@gmail.com>
2024-06-24 21:19:38 +0300

    Protected Route.

--- opespace-ui/src/App.js
+++ opespace-ui/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/login/" element={<div>Login</div>} />
+      </Routes>
     </>
   );
 }
