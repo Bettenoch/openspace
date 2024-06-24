2013099b44989e783152e366aa01cf809409ac9c
Bettenoch <bettenoch254@gmail.com>
2024-06-24 22:22:54 +0300

    register login and registration pages.

--- opespace-ui/src/App.js
+++ opespace-ui/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import ProtectedRoute from './routes/ProtectedRoute';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
           </ProtectedRoute>
         } />
 
-        <Route path="/login/" element={<div>Login</div>} />
+        <Route path="/login/" element={<Login/>} />
+        <Route path="/register/" element={<Registration/>} />
       </Routes>
     </>
   );
